b7118bf206631e29f4e1d58a491f953b8d6b8baa
Yunus Kavcuk <65766059+ByKas22@users.noreply.github.com>
2020-05-26 15:11:45 +0300

    Update Odev5.java

--- Odev5.java
+++ Odev5.java
@@ -1,26 +1,24 @@
+/*Klavyeden girilen kelime yada metni tersten yazdıran paramtereli metod kullanımı*/
 
 package odev.pkg5;
-
-import java.util.Scanner;
-
-public class Odev5 {
-
+import java.util.Scanner; //scanner sınıfını kütüphaneye ekledik.
+public class Odev5 
+{
     public static void main(String[] args) 
     {
-        String kelime;
-        Scanner girdi = new Scanner(System.in);
-        
-        System.out.println("Lütfen Kelimeyi Giriniz..:");
-        kelime=girdi.nextLine();
-        System.out.println("Kelimenin Tersi..:" + tersten(kelime));
+        String kelime; //string türünde değişken oluşturduk.
+        Scanner girdi = new Scanner(System.in); //değer girebilmek için scanner sınıfını çağırdık.
+        System.out.println("Lütfen Kelimeyi Giriniz..:"); //ekrana yazdırdık.
+        kelime=girdi.nextLine(); //klavyeden değer aldı.(Line sayesinde metin girsek bile tersten yazar)
+        System.out.println("Kelimenin Tersi..:" + tersten(kelime)); //ekrana kelimenin tersini yazdırdık.
     }
-    static String tersten(String kelime)
+    static String tersten(String kelime) //tersten adında metod oluşturduk.(parametreli)
     {
-        String tersi="";
-            for (int i = kelime.length()-1; i >=0 ; i--) 
+        String tersi=""; //string veri türünde atama yapabileceğimiz değişken oluşturduk.
+            for (int i = kelime.length()-1; i >=0 ; i--) //bu döngüyle kelimemizi tersten yazdırdık.
             {
-                tersi = tersi + kelime.charAt(i);
+                tersi = tersi + kelime.charAt(i); //her dizideki kelimeyi tersi değişkenine atayıp toplar.
             }
-    return tersi;
+    return tersi; //geri değer döndürür.
     }  
 }
